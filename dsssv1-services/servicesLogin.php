b7c749a694c335e59117855afa9f3469e6b343b8
Mark Andres <mandres12@gmail.com>
2018-07-03 17:30:21 -0500

    Removed use of login post variable.

--- dsssv1-services/servicesLogin.php
+++ dsssv1-services/servicesLogin.php
@@ -1,11 +1,24 @@
 <?php
 require('../Support/utils.php');
 $error = '';
-if (isset($_POST['login'])) {
-    startSession();
-    $username = trim($_POST['username']);
-    $password = trim($_POST['pwd']);
-    $_SESSION['timezone'] = trim($_POST['timezone']);
-    $redirectName = '';
-    require_once ('../Includes/authenticate.inc.php');
+startSession();
+$username = trim($_POST['username']);
+$password = trim($_POST['pwd']);
+$_SESSION['timezone'] = trim($_POST['timezone']);
+$redirectName = '';
+require_once ('../Includes/authenticate.inc.php');
+$result = array();
+if (isset($error) && $error != '') {
+    $result['error'] = $error;
+} else {
+    $result['start'] = $_SESSION['start'];
+    $result['username'] = $_SESSION['username'];
+    $result['store'] = $_SESSION['store'];
+    $result['storeId'] = $_SESSION['store_id'];
+    $result['role'] = $_SESSION['role'];
+    $result['lastname'] = $_SESSION['lastname'];
+    $result['firstname'] = $_SESSION['firstname'];
+    $result['nickname'] = $_SESSION['nickname'];
+    $result['staff_id'] = $_SESSION['staff_id'];
 }
+echo json_encode($result);
